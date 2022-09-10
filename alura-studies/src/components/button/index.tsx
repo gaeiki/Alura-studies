59783bd0b15653c6804119023da50df4800ccf0c
Gabriel Eiki Oshiro <gabueiki@gmail.com>
2022-09-10 20:32:16 -0300

    Correção do button

--- alura-studies/src/components/button/index.tsx
+++ alura-studies/src/components/button/index.tsx
@@ -13,15 +13,4 @@ function Botao({ onClick, type, children}: Props){
     )
 }
 
-// class Botao1 extends React.Component<{children: any, type?: "button" | "submit" | "reset" | undefined, onClick?: () => void}>{
-//     render(){
-//         const {type = "button", onClick} = this.props
-//         return(
-//             <button onClick={onClick} type={type} className={style.botao}>
-//                 {this.props.children}
-//             </button>
-//         )
-//     }
-// }
-
 export default Botao;
